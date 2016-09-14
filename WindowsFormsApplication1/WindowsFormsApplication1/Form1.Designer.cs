6e9cc59954dbd46149bf48f2d15bdf0d3565c0d8
lakmaleranda <lakmaleranda@gmail.com>
2016-09-14 21:44:03 +0530

    jk

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(343, 286);
+            this.ClientSize = new System.Drawing.Size(312, 241);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
